3aef349b4354eb548675375a9ad69e4ac2d1e606
james-99k <jamesgasty@gmail.com>
2021-05-08 19:35:05 +0800

    portfolio website

--- script.js
+++ script.js
@@ -6,10 +6,59 @@ $(document).ready(function(){
         else{
             $('.navbar').removeClass("sticky");
         }
+        if (this.scrollY > 500){
+            $('.scroll-up-button').addClass("show");
+        }
+        else {
+            $('.scroll-up-button').removeClass("show");
+        }
     });
+
+    // slide up
+    $('.scroll-up-button').click(function(){
+        $('html').animate({scrollTop: 0})
+    });
+
     // toggle menu 
     $('.menu__button').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu__button i').toggleClass("active");
+    });
+
+    //typing animation
+    var typed = new Typed(".typing", {
+        strings: ["Programmer", "Developer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Programmer", "Developer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
     });
 });
